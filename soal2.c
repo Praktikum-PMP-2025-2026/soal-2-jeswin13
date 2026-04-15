377df235780faf0a195a3ceec05609dcbf7105c2
jeswin13 <jeswindersingh36@gmail.com>
2026-04-15 08:23:39 +0700

    Implement recursive function for Collatz conjecture

--- soal2.c
+++ soal2.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
-int main(void) {
+int rekursi(int n) {
+    int i = 0;
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        i = i+1; 
+    }
+    return i; 
+}
+
+int main() {
+    int t;
+    int n;
+    scanf("%d", &t);
+    for (int j = 0; j < t; j++) {
+        scanf("%d", &n);
+        int langkah = rekursi(n);
+        printf("LANGKAH %d\n", langkah);
+    }
     return 0;
 }
+
